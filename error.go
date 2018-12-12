1ccbfe50b2bd7eab91594f76042da7aafbceee5d
Orkun Karaduman <orkunkaraduman@gmail.com>
2018-12-12 14:56:09 +0300

    re-arranged error.go

--- error.go
+++ error.go
@@ -1,22 +1,8 @@
 package criticalsection
 
+import "errors"
+
 // Library specific errors.
 var (
-	ErrPanic     = NewError("panic")
-	ErrNotLocked = NewError("not locked")
+	ErrNotLocked = errors.New("not locked")
 )
-
-// Error struct is base of library specific errors.
-type Error struct {
-	s string
-}
-
-// NewError returns a new Error.
-func NewError(text string) *Error {
-	return &Error{text}
-}
-
-// Error implements error interface.
-func (e *Error) Error() string {
-	return e.s
-}
