b61691561e665dbaca7e16e7b50c1b93002f398b
Orkun Karaduman <orkunkaraduman@gmail.com>
2018-12-17 13:47:59 +0300

    adjusted sleepcycle & added section feature (NewSection and LockSection)

--- criticalsection.go
+++ criticalsection.go
@@ -6,19 +6,21 @@ import (
 	"time"
 )
 
-const maxSleepCycle = 14
+const maxSleepCycle = 6
 
 // A CriticalSection is a kind of lock like mutex. But it doesn't block
-// first locked goroutine again.
+// first locked section again.
 //
 // A CriticalSection must not be copied after first use.
 type CriticalSection struct {
 	mu sync.Mutex
 	v  int32
 	id uint64
+	sc Section
 }
 
-// Lock locks cs. If the lock is already in use different goroutine, the calling
+// Lock locks cs.
+// If the lock is already in use different goroutine, the different
 // goroutine blocks until the CriticalSection is available.
 func (cs *CriticalSection) Lock() {
 	id := getGID()
@@ -33,7 +35,7 @@ func (cs *CriticalSection) Lock() {
 		}
 		cs.mu.Unlock()
 		runtime.Gosched()
-		time.Sleep(time.Duration(1<<i) * 1 * time.Nanosecond)
+		time.Sleep(time.Duration(1<<i) * 1 * time.Millisecond)
 		i++
 		if i >= maxSleepCycle {
 			i = 0
@@ -41,7 +43,8 @@ func (cs *CriticalSection) Lock() {
 	}
 }
 
-// Unlock unlocks cs. It panics if cs is not locked on entry to Unlock.
+// Unlock unlocks cs.
+// It panics if cs is not locked on entry to Unlock.
 func (cs *CriticalSection) Unlock() {
 	cs.mu.Lock()
 	if cs.v < 0 {
@@ -49,5 +52,32 @@ func (cs *CriticalSection) Unlock() {
 		panic(ErrNotLocked)
 	}
 	cs.v--
+	if cs.v == 0 {
+		cs.id = 0
+		cs.sc = 0
+	}
 	cs.mu.Unlock()
+}
+
+// LockSection locks cs by given section.
+// If the lock is already in use different section, the different
+// section blocks until the CriticalSection is available.
+func (cs *CriticalSection) LockSection(sc Section) {
+	i := uint(0)
+	for {
+		cs.mu.Lock()
+		if cs.v == 0 || cs.sc == sc {
+			cs.v++
+			cs.sc = sc
+			cs.mu.Unlock()
+			break
+		}
+		cs.mu.Unlock()
+		runtime.Gosched()
+		time.Sleep(time.Duration(1<<i) * 1 * time.Millisecond)
+		i++
+		if i >= maxSleepCycle {
+			i = 0
+		}
+	}
 }
